d6b34acb380684866a8bc989d9686fe217e42c53
Gustavo Reyes <tavoberry@gmail.com>
2016-02-07 22:17:51 -0600

    Proyecto inicial

---
+++ CreacionPizza/CreacionPizza/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  CreacionPizza
+//
+//  Created by Gustavo Reyes on 06/02/16.
+//  Copyright © 2016 Gustavo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var tamaño: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CreacionPizza/CreacionPizza/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  CreacionPizza
+//
+//  Created by Gustavo Reyes on 06/02/16.
+//  Copyright © 2016 Gustavo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var tipoMasa: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
